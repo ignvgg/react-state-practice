9a3ea18011819b2fd7956781fd37e731819951d3
Nacho Viggiani <ign.v@hotmail.com>
2022-09-27 17:31:20 +0000

    updated app
    E

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      entities: [
+        {
+          name: 'The Dude',
+          likes: ['wine', 'comfy clothes']
+        }, {
+          name: 'Homer',
+          likes: ['donuts', 'beer']
+        }, {
+          name: 'Fry',
+          likes: ['cyclops', 'dogs']
+        }, {
+          name: 'Nacho',
+          likes: ['javascript', 'typescript']
+        }
+      ]
+    };
+    
+    setTimeout(() => {
+      const randEnt = Math.floor(
+        Math.random() *
+        this.state.entities.length
+      );
+      
+      const hobbyIndex = Math.floor(
+        Math.random() *
+        this.state.entities[randEnt].length
+      );
+      
+      const entities = this.state.entities.map((ent, i) => {
+        if (i === randEnt) {
+          const likes = [...ent.likes];
+          likes.splice(hobbyIndex, 1);
+          return {
+            ...ent,
+            likes
+          };
+        }
+        
+        return ent;
+      });
+      this.setState({entities});
+    }, 3000);
+  }
+  
+  render() {
+    const entities = this.state.entities.map((instructor, index) => (
+      <li key={index}>
+        <h3>{instructor.name}</h3>
+        <h4>Likes: {instructor.likes.join(", ")}</h4>
+      </li>
+    ));
+    return (
+      <div className="App">
+        <ul>
+          {entities}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
